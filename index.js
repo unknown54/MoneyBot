6f4751299932b0fa72da95af0ef9e442c631200c
Erik <nuclear.ace@gmail.com>
2016-09-13 18:17:09 -0400

    add weather command

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var DiscordBot = require('./bot').DiscordBot
+var DiscordBot = require('./lib/bot').DiscordBot
+var config = require('./lib/config');
 
-var bot = new DiscordBot('', '');
+config.load(config => {
+    var bot = new DiscordBot('', '', config);
+});
+
 
 process.on('SIGINT', () => {
     bot.io.setPresence({
