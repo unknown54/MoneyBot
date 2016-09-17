7bd8893804353c0bd5a81fee26aa12b746bd8084
Erik <nuclear.ace@gmail.com>
2016-09-17 11:01:49 -0400

    add role based command checking

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ var config = require('./lib/config');
 var bot = null;
 
 config.load(config => {
-    bot = new DiscordBot('', config);
+    bot = new DiscordBot(config);
 });
 
-
 process.on('SIGINT', () => {
+    // Bot has 1 second to clean up before we exit
+    setTimeout(process.exit, 1000);
+
     bot.cleanUp(() => {
         console.log('Clean up before exit');
     });
-
-    // Bot has 1 second to clean up before we exit
-    setTimeout(process.exit, 1000);
 })
